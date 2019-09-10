851f0771aecd3e32858aa7c0e0ed94b355318831
npolanco47 <npolanco@mail.csuchico.edu>
2019-09-09 17:24:31 -0700

    Doing small changes

--- madlibs.cpp
+++ madlibs.cpp
@@ -1,9 +1,32 @@
-//Authors: 
+//Nixon Polanco
 #include <iostream>
 #include <string>
-
+using std::cin;
+using std::cout;
+using std::string;
+using std::endl;
 int main()
 {
+string verb;
+int number;
+string adjective;
+string noun;
+cout <<"Enter a first verb: ";
+cin >> verb; 
+cout << endl;
+cout <<"Enter a whole number:";
+cin >> number;
+cout << endl;
+cout <<"Enter an adjective:";
+cin >> adjective;
+cout << endl;
+cout <<"Enter a noun:";
+cin >> noun;
+cout << endl;
 
+cout <<"Your Mad Libs Story:"<< endl;
+
+
+cout <<"The girl "<< verb <<" to her class she was exhausted from the "<< number <<" tests she had to take. But all in all she maintained a "<< adjective <<" attitude and walked to "<< noun <<"." << endl;
   return 0;
 }
